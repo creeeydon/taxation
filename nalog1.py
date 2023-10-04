2c5bca492c8493267b06395fa2b5b4d306fc1764
creeeydon <saha.calganov3@mail.ru>
2023-10-04 18:12:09 +0700

    updated labs

--- nalog1.py
+++ nalog1.py
@@ -2,106 +2,89 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 
-st.title('Лабораторная работа №2. Земельный налог')
+st.title('Лабораторная работа №1. Транспортный налог')
 st.divider()
-
 
 def user_input_features():
     st.sidebar.header('Введите значения')
     st.sidebar.divider()
-    date_start = st.sidebar.date_input('Дата установки права собственности', value=datetime(2021, 1, 1),
-                                       format="DD/MM/YYYY")
-    date_stop = st.sidebar.date_input('Дата прекращения права собственности', value=datetime(2021, 12, 31),
-                                      format="DD/MM/YYYY")
-    part = st.sidebar.number_input('Доля владения, %', 1, 100, 100, 1)
-    cost = st.sidebar.number_input('Кадастровая стоимость, руб.', 1, 10000000000, 1000000, 1000)
-    rate = st.sidebar.radio('Ставка', [
-        "0,3% / Селькохозяйственное назначение, жилищный фонд, жилищное строительство, личное хозяйство",
-        "1,5% / Прочее"])
-    purpose = st.sidebar.radio('Назначение (Повышающий коэффициент)',
-                               ["Жилищное строительство (превышение 3х летнего срока строительства) / x4",
-                                "Жилищное строительство / x2",
-                                "Индивидуальное жилищное строительство (по истечении 10 лет) / x2",
-                                "Другое / x1"], index=3)
-    data = {'date_start': date_start,
-            'date_stop': date_stop,
-            'part': part,
+    date_reg = st.sidebar.date_input('Дата регистрации', value=datetime(2021, 8, 29), format="DD/MM/YYYY")
+    date_vip = st.sidebar.date_input('Дата выпуска', value=datetime(2020, 1, 10), format="DD/MM/YYYY")
+    cost = st.sidebar.number_input('Стоимость', 1, 10000000000, 1150000, 1000)
+    power = st.sidebar.number_input('Мощность двигателя', 1, 1500, 100)
+    data = {'date_reg': date_reg,
+            'date_vip': date_vip,
             'cost': cost,
-            'rate': rate,
-            'purpose': purpose}
+            'power': power}
     table = pd.DataFrame(data, index=[0])
     return table
 
-#     x = 0.003 if str(rate).count("0,3")==1 else x = 0.015
-#     return x
-
-
-def calculate_rate(rate):
-    if str(rate).count("0,3") == 1:
-        return 0.003
-    elif str(rate).count("1,5") == 1:
-        return 0.015
-    else:
-        return 0
-
-
-def calculate_koef(purpose):
-    if str(purpose).count("x4") == 1:
-        return 4
-    elif str(purpose).count("x2") == 1:
-        return 2
-    elif str(purpose).count("x1") == 1:
-        return 1
-    else:
-        return 0
-
-
 def calc_tax(input: pd.DataFrame):
-    date_start = input.at[0, 'date_start']
-    date_stop = input.at[0, 'date_stop']
-    part = input.at[0, 'part'] / 100
+    date_reg = input.at[0, 'date_reg']
+    date_vip = input.at[0, 'date_vip']
     cost = input.at[0, 'cost']
-    rate = calculate_rate(input.at[0, 'rate'])
-    coeff = calculate_koef(input.at[0, 'purpose'])
+    power = input.at[0, 'power']
+    koef = 1.0
+    stavka = 1.0
 
-    months_having = [3, 3, 3, 3]
-    month_start = date_start.month
-    if date_start.day > 15:
-        month_start += 1
-    month_stop = date_stop.month
-    if date_stop.day <= 15:
-        month_stop -= 1
+    if power > 250:
+        stavka = 110.0
 
-    for i in range(1, 13):
-        if i < month_start:
-            months_having[int((i - 1) / 3)] -= 1
-        if i > month_stop:
-            months_having[int((i - 1) / 3)] -= 1
+    if power <= 250:
+        stavka = 47.0
 
-    avans = [0.0, 0.0, 0.0, 0.0]
-    summary = 0.0
-    for i in range(len(avans)):
-        avans[i] = 1 / 4 * cost * rate * (months_having[i] / 3) * part * coeff
-        summary += avans[i]
+    if power <= 200:
+        stavka = 28.0
 
-    input_dict = {'Ставка': rate,
-                  'Повышающий коэф': coeff,
-                  'Доля': "{:.0%}".format(part),
-                  'Кад.стоимость': cost,
-                  'Кварталы': ','.join(str(i) for i in months_having)
-                  }
-    input_table = pd.DataFrame(input_dict, index=[""])
-    input_table.set_index('Ставка', inplace=True)
+    if power <= 150:
+        stavka = 14.0
+
+    if power <= 100:
+        stavka = 8.0
+
+    if 10000000 <= cost <= 15000000 and 2023 - date_vip.year <= 10:
+        koef = 3.0
+
+    if 15000000 <= cost and 2023 - date_vip.year <= 20:
+        koef = 3.0
+
+    month = date_reg.month-1
+    if date_reg.day > 15:
+        month += 1
+
+    monthHaving = ((12 - month) / 12.0)
+    tax = power * stavka * monthHaving * koef
+    tempVar = {'Ставка': stavka,
+                'Пропустили': month,
+                'Повышающий коэффициент': koef,
+                'Владение': monthHaving}
+    temp = pd.DataFrame(tempVar, index=[""])
+    temp.set_index('Ставка',inplace=True)
     st.header('Составляющие')
-    st.write(input_table)
-    tax = [summary, avans[0], avans[1], avans[2], avans[3]]
-    return tax
+    st.write(temp)
 
+    avans = [0.0, 0.0, 0.0]
+    i = 0
+    for j in range(3, 10, 3):
+        if month - j < 0:
+            avans[i] = 0.25 * stavka * power * ((j - month) / 3.0) * koef
+            month = j
+            i = i + 1
+        else:
+            i = i + 1
+
+    sumAvan = 0
+    for i in range(len(avans)):
+        sumAvan+=avans[i]
+
+    lastTax = tax - sumAvan
+    tax = [tax, avans[0], avans[1], avans[2], lastTax]
+    return tax
 
 df = user_input_features()
 tax = calc_tax(df)
 
-st.header('Земельный налог')
+st.header('Транспортный налог')
 st.code(str(tax.pop(0)) + " рублей")
 st.header('Выходные данные')
 prepaids = {'Кв. 1': tax.pop(0),
